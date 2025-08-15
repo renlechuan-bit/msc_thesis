e9e3446e0016200068fd8268a9eb323e880dc017
vinisalazar <17276653+vinisalazar@users.noreply.github.com>
2025-08-15 14:45:31 +1000

    Modify default command

--- code/plot_map.sh
+++ code/plot_map.sh
@@ -4,4 +4,4 @@ sinteractive -p cascade,interactive,sapphire --time=12:00:00 --cpus-per-task=1 -
 conda create -n plot_map -c conda-forge python=3.10 -y
 conda activate plot_map
 conda install -c conda-forge cartopy matplotlib pandas numpy scikit-learn shapely pyproj proj geos -y
-python plot_map.py -i /data/gpfs/projects/punim2504/msc_thesis/data/metadata_1446.csv -c Prov -p k_8_adjusted --drop-dup
+python plot_map.py -i /data/gpfs/projects/punim2504/msc_thesis/data/metadata_1446.csv --drop-dup --no-palette --cmap tab20b -o ../plots
